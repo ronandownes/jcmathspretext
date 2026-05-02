b1b005ec3e27a6ea1c48e1f37099a6e5ebea8100
Ronan Downes <ronandownes@gmail.com>
2026-05-02 23:03:51 +0100

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,221 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "sec-lines",
+  "level": "1",
+  "url": "sec-lines.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Lines",
+  "body": " Lines   Line Line is undefined. is short for straight line. Take a fixed plane Undefined term , once and for all, and consider just lines that lie in it. The plane and the lines are sets Undefined term of points Undefined term . Each line is a subset of the plane, i.e. each element of a line is a point of the plane. Each line is endless, extending forever in both directions. Each line has infinitely-many points. The points on a line can be taken to be ordered along the line in a natural way. As a consequence, given any three distinct points on a line, exactly one of them lies between the other two. Points that are not on a given line can be said to be on one or other side of the line. The sides of a line are sometimes referred to as half-planes .   Notation  We denote points by roman capital letters , , , etc., and lines by lower-case roman letters , , , etc.   Axioms are statements we will accept as true. An axiom is a statement accepted without proof, as a basis for argument. A theorem is a statement deduced from the axioms by logical argument.   Two Points Axiom   There is exactly one line through any two given points. (We denote the line through and by .)      The line segment  is the part of the line between and (including the endpoints). The point divides the line into two pieces, called rays . The point lies between all points of one ray and all points of the other. We denote the ray that starts at and passes through by . Rays are sometimes referred to as half-lines .  Three points usually determine three different lines.      If three or more points lie on a single line, we say they are collinear .      Let , and be points that are not collinear. The triangle  is the piece of the plane enclosed by the three line segments , and . The segments are called its sides , and the points are called its vertices (singular vertex ).    "
+},
+{
+  "id": "sec-lines-2",
+  "level": "2",
+  "url": "sec-lines.html#sec-lines-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Line plane sets points subset between side half-planes "
+},
+{
+  "id": "conv-point-line-notation",
+  "level": "2",
+  "url": "sec-lines.html#conv-point-line-notation",
+  "type": "Convention",
+  "number": "1.1.1",
+  "title": "Notation.",
+  "body": " Notation  We denote points by roman capital letters , , , etc., and lines by lower-case roman letters , , , etc.  "
+},
+{
+  "id": "sec-lines-4",
+  "level": "2",
+  "url": "sec-lines.html#sec-lines-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "axiom theorem "
+},
+{
+  "id": "ax-two-points",
+  "level": "2",
+  "url": "sec-lines.html#ax-two-points",
+  "type": "Axiom",
+  "number": "1.1.2",
+  "title": "Two Points Axiom.",
+  "body": " Two Points Axiom   There is exactly one line through any two given points. (We denote the line through and by .)   "
+},
+{
+  "id": "def-line-segment",
+  "level": "2",
+  "url": "sec-lines.html#def-line-segment",
+  "type": "Definition",
+  "number": "1.1.3",
+  "title": "",
+  "body": "  The line segment  is the part of the line between and (including the endpoints). The point divides the line into two pieces, called rays . The point lies between all points of one ray and all points of the other. We denote the ray that starts at and passes through by . Rays are sometimes referred to as half-lines .  Three points usually determine three different lines.   "
+},
+{
+  "id": "def-collinear",
+  "level": "2",
+  "url": "sec-lines.html#def-collinear",
+  "type": "Definition",
+  "number": "1.1.4",
+  "title": "",
+  "body": "  If three or more points lie on a single line, we say they are collinear .   "
+},
+{
+  "id": "def-triangle",
+  "level": "2",
+  "url": "sec-lines.html#def-triangle",
+  "type": "Definition",
+  "number": "1.1.5",
+  "title": "",
+  "body": "  Let , and be points that are not collinear. The triangle  is the piece of the plane enclosed by the three line segments , and . The segments are called its sides , and the points are called its vertices (singular vertex ).   "
+},
+{
+  "id": "sec-distance",
+  "level": "1",
+  "url": "sec-distance.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Distance",
+  "body": " Distance  We denote the set of all real numbers Undefined term by .    We denote the distance Undefined term between the points and by . We define the length of the segment to be .  We often denote the lengths of the three sides of a triangle by , , and . The usual thing for a triangle is to take , i.e. the length of the side opposite the vertex , and similarly and .     Ruler Axiom   The distance between points has the following properties:   the distance is never negative;  ;  if lies on , between and , then ;  (marking off a distance) given any ray from , and given any real number , there is a unique point on the ray whose distance from is .       The midpoint of the segment is the point of the segment with Students may notice that the first equality implies the second.      "
+},
+{
+  "id": "def-distance",
+  "level": "2",
+  "url": "sec-distance.html#def-distance",
+  "type": "Definition",
+  "number": "1.2.1",
+  "title": "",
+  "body": "  We denote the distance Undefined term between the points and by . We define the length of the segment to be .  We often denote the lengths of the three sides of a triangle by , , and . The usual thing for a triangle is to take , i.e. the length of the side opposite the vertex , and similarly and .   "
+},
+{
+  "id": "ax-ruler",
+  "level": "2",
+  "url": "sec-distance.html#ax-ruler",
+  "type": "Axiom",
+  "number": "1.2.2",
+  "title": "Ruler Axiom.",
+  "body": " Ruler Axiom   The distance between points has the following properties:   the distance is never negative;  ;  if lies on , between and , then ;  (marking off a distance) given any ray from , and given any real number , there is a unique point on the ray whose distance from is .    "
+},
+{
+  "id": "def-midpoint",
+  "level": "2",
+  "url": "sec-distance.html#def-midpoint",
+  "type": "Definition",
+  "number": "1.2.3",
+  "title": "",
+  "body": "  The midpoint of the segment is the point of the segment with Students may notice that the first equality implies the second.     "
+},
+{
+  "id": "sec-angles",
+  "level": "1",
+  "url": "sec-angles.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Angles",
+  "body": " Angles    A subset of the plane is convex if it contains the whole segment that connects any two of its points.    For example, one side of any line is a convex set, and triangles are convex sets.  We do not define the term angle formally. Instead we say: There are things called angles . To each angle is associated:  a unique point , called its vertex ;  two rays and , both starting at the vertex, and called the arms of the angle;  a piece of the plane called the inside of the angle.    An angle is either a null angle, an ordinary angle, a straight angle, a reflex angle or a full angle. Unless otherwise specified, you may take it that any angle we talk about is an ordinary angle.    An angle is a null angle if its arms coincide with one another and its inside is the empty set.      An angle is an ordinary angle if its arms are not on one line, and its inside is a convex set.      An angle is a straight angle if its arms are the two halves of one line, and its inside is one of the sides of that line.      An angle is a reflex angle if its arms are not on one line, and its inside is not a convex set.      An angle is a full angle if its arms coincide with one another and its inside is the rest of the plane.      Suppose that , , and are three noncollinear points. We denote the (ordinary) angle with arms and by (and also by ). We shall also use the notation to refer to straight angles, where , , are collinear, and lies between and (either side could be the inside of this angle).  Sometimes we want to refer to an angle without naming points, and in that case we use lower-case Greek letters, , etc.    "
+},
+{
+  "id": "def-convex",
+  "level": "2",
+  "url": "sec-angles.html#def-convex",
+  "type": "Definition",
+  "number": "1.3.1",
+  "title": "",
+  "body": "  A subset of the plane is convex if it contains the whole segment that connects any two of its points.   "
+},
+{
+  "id": "sec-angles-4",
+  "level": "2",
+  "url": "sec-angles.html#sec-angles-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "angles vertex arms inside "
+},
+{
+  "id": "def-null-angle",
+  "level": "2",
+  "url": "sec-angles.html#def-null-angle",
+  "type": "Definition",
+  "number": "1.3.2",
+  "title": "",
+  "body": "  An angle is a null angle if its arms coincide with one another and its inside is the empty set.   "
+},
+{
+  "id": "def-ordinary-angle",
+  "level": "2",
+  "url": "sec-angles.html#def-ordinary-angle",
+  "type": "Definition",
+  "number": "1.3.3",
+  "title": "",
+  "body": "  An angle is an ordinary angle if its arms are not on one line, and its inside is a convex set.   "
+},
+{
+  "id": "def-straight-angle",
+  "level": "2",
+  "url": "sec-angles.html#def-straight-angle",
+  "type": "Definition",
+  "number": "1.3.4",
+  "title": "",
+  "body": "  An angle is a straight angle if its arms are the two halves of one line, and its inside is one of the sides of that line.   "
+},
+{
+  "id": "def-reflex-angle",
+  "level": "2",
+  "url": "sec-angles.html#def-reflex-angle",
+  "type": "Definition",
+  "number": "1.3.5",
+  "title": "",
+  "body": "  An angle is a reflex angle if its arms are not on one line, and its inside is not a convex set.   "
+},
+{
+  "id": "def-full-angle",
+  "level": "2",
+  "url": "sec-angles.html#def-full-angle",
+  "type": "Definition",
+  "number": "1.3.6",
+  "title": "",
+  "body": "  An angle is a full angle if its arms coincide with one another and its inside is the rest of the plane.   "
+},
+{
+  "id": "def-angle-notation",
+  "level": "2",
+  "url": "sec-angles.html#def-angle-notation",
+  "type": "Definition",
+  "number": "1.3.7",
+  "title": "",
+  "body": "  Suppose that , , and are three noncollinear points. We denote the (ordinary) angle with arms and by (and also by ). We shall also use the notation to refer to straight angles, where , , are collinear, and lies between and (either side could be the inside of this angle).  Sometimes we want to refer to an angle without naming points, and in that case we use lower-case Greek letters, , etc.   "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
